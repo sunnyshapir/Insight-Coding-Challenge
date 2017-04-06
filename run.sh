f87fae4ef097806ea72742ff77830ddc7a82b5dd
sunnyshapir <sunnyshapir@users.noreply.github.com>
2017-04-06 00:44:55 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-javac ./src/ProcessLog.java ./src/MappedPair.java ./src/LoginAttempt.java 
+javac ./src/ProcessLog.java ./src/MappedPair.java ./src/LoginAttempt.java ./src/TimePair.java
 java -cp ./src/ ProcessLog "./log_input/log.txt" "./log_output/hosts.txt" "./log_output/resources.txt" "./log_output/hours.txt" "./log_output/blocked.txt"
